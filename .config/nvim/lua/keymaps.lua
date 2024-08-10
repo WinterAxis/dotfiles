d35cea1435a77ded5cd35d8c8594fe5c476c67d3
WinterAxis <nate.a.alden@gmail.com>
2024-08-10 02:16:39 -0400

    keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -25,12 +25,13 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --  NOTE: Because of tmux keybinds used as CTRL+ALT+<hjkl>
+--  But also not using because they are going to be used for quick fix
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -46,9 +47,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- [[ Remaps ]]
+
+-- Center when possible
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Paste over selection, pasted text to void register
+vim.keymap.set('x', '<leader>p', [["_dP]])
+-- Delete to void register
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+
 -- [[ Custom Keybinds ]]
 --
-vim.keymap.set('n', '<leader>pv', ':Explore<enter>', { desc = '[P]roject [V]iew' })
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[P]roject [V]iew' })
 
---
+-- Quickfix
+vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>zz', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>zz', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-h>', '<cmd>lnext<CR>zz', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<cmd>lprev<CR>zz', { desc = 'Move focus to the right window' })
+
 -- vim: ts=2 sts=2 sw=2 et
