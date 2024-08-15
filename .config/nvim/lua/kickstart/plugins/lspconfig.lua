ae54c21eddd721a8893c1df8c1dca2f3550a0602
WinterAxis <nate.a.alden@gmail.com>
2024-08-15 20:36:00 +0000

    lsp

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -169,6 +169,8 @@ return {
         -- clangd = {},
         gopls = {},
         -- pyright = {},
+        jedi_language_server = {},
+        -- ruff = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
